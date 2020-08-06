adfadb2f607a2ed3ddd3f216f0dc73cecf880767
philips77 <aleksander.nowakowski@nordicsemi.no>
2020-08-06 09:59:14 +0200

    UI tests fixed

--- Example/Pods/Target Support Files/Pods-nRFBlinky-nRFBlinky_UITests/Pods-nRFBlinky-nRFBlinky_UITests-umbrella.h
+++ Example/Pods/Target Support Files/Pods-nRFBlinky-nRFBlinky_UITests/Pods-nRFBlinky-nRFBlinky_UITests-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_nRFBlinky_UITestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_nRFBlinky_UITestsVersionString[];
+FOUNDATION_EXPORT double Pods_nRFBlinky_nRFBlinky_UITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_nRFBlinky_nRFBlinky_UITestsVersionString[];
 
