4debc47d98fa5a3e5b91a8545f047e7510f83dcd
philips77 <aleksander.nowakowski@nordicsemi.no>
2021-08-11 10:55:23 +0200

    Retrieval tests fixed

--- Example/Tests/RetrievingDevicesTest.swift
+++ Example/Tests/RetrievingDevicesTest.swift
@@ -58,18 +58,27 @@ class RetrievingDevicesTest: XCTestCase {
         blinky.simulateMacChange()
         hrm.simulateMacChange()
         
-        // Creating a manager with CBMCentralManagerSimpleMockKey = true makes the
-        // manager available powered on immediately, without the need to wait 10 ms.
-        let manager = CBMCentralManagerFactory.instance(delegate: nil, queue: nil,
-                                                        options: [CBMCentralManagerSimpleMockKey : true],
-                                                        forceMock: true)
-        // Initially, none of the devices was cached.
+        let managerPoweredOn = XCTestExpectation(description: "Powered On")
+        
+        // Define the CBCentralManagerDelegate, which will wait until the manager is ready.
+        let delegate = CBMCentralManagerDelegateProxy()
+        delegate.didUpdateState = { _ in managerPoweredOn.fulfill() }
+        
+        // Create an instance of mock CBMCentralManager.
+        let manager = CBMCentralManagerMock(delegate: delegate, queue: nil)
+        
+        // Wait until the manager is initialized.
+        wait(for: [managerPoweredOn], timeout: 1.0)
+        
+        // Initially, none of the devices should be retrievable, as they have not been scanned.
         let list1 = manager.retrievePeripherals(withIdentifiers: [blinky.identifier, hrm.identifier])
         XCTAssertTrue(list1.isEmpty)
         
-        // Simulate a situation when a different app on the iPhone scanned for Blinky
-        // which cached the device. This should also work when the device is out of
-        // range.
+        // Simulate a situation when a different app on the iPhone scanned the Blinky device
+        // causing it to be cached.
+        //
+        // Note: This should also work when the device is out of range, as undefined time can pass
+        //       between retrievals, so it could have been close for a moment, and taken away again.
         blinky.simulateCaching()
         
         // Now the Blinky should be available for retrieval.
@@ -80,35 +89,130 @@ class RetrievingDevicesTest: XCTestCase {
     
     func testRetrievalAfterScanning() throws {
         // Set up the devices in range.
-        hrm.simulateProximityChange(.immediate)
+        thingy.simulateMacChange()
+        thingy.simulateProximityChange(.immediate)
         
-        let stateUpdated = XCTestExpectation(description: "State Updated")
-        let retrieved = XCTestExpectation(description: "Retrieved")
+        let managerPoweredOn = XCTestExpectation(description: "Powered On")
+        let thingyFound = XCTestExpectation(description: "Found")
+        let thingyRetrieved = XCTestExpectation(description: "Retrieved")
         
+        // Define the CBCentralManagerDelegate, which will wait until the manager is ready
+        // and will check the successful retrieval.
         let delegate = CBMCentralManagerDelegateProxy()
-        delegate.didUpdateState = { _ in stateUpdated.fulfill() }
-        delegate.didDiscoverPeripheral = { manager, _, _, _ in
+        delegate.didUpdateState = { _ in managerPoweredOn.fulfill() }
+        delegate.didDiscoverPeripheral = { manager, peripheral, _, _ in
+            guard peripheral.identifier == thingy.identifier else { return }
+            thingyFound.fulfill()
+            
             // Now it should be available for retrieval.
-            let list = manager.retrievePeripherals(withIdentifiers: [hrm.identifier])
+            let list = manager.retrievePeripherals(withIdentifiers: [thingy.identifier])
             XCTAssertFalse(list.isEmpty)
             
             if !list.isEmpty {
-                retrieved.fulfill()
+                thingyRetrieved.fulfill()
             }
         }
         
-        // Creating a manager with CBMCentralManagerSimpleMockKey = true makes the
-        // manager available powered on immediately, without the need to wait 10 ms.
-        let manager = CBMCentralManagerFactory.instance(delegate: delegate, queue: nil,
-                                                        options: [CBMCentralManagerSimpleMockKey : true],
-                                                        forceMock: true)
-        // Initially, none of the devices was cached.
-        let list = manager.retrievePeripherals(withIdentifiers: [hrm.identifier])
+        // Create an instance of a mock CBMCentralManager.
+        let manager = CBMCentralManagerMock(delegate: delegate, queue: nil)
+        
+        // Wait until the manager is initialized.
+        wait(for: [managerPoweredOn], timeout: 1.0)
+        
+        // Initially, none of the devices should be retrievable, as they have not been scanned.
+        let list = manager.retrievePeripherals(withIdentifiers: [thingy.identifier])
         XCTAssertTrue(list.isEmpty)
         
+        // Start scanning for any device.
         manager.scanForPeripherals(withServices: nil)
         
-        wait(for: [stateUpdated, retrieved], timeout: 1.0)
+        // Wait until the device is found and could be retrieved.
+        wait(for: [thingyFound, thingyRetrieved], timeout: 1.0)
+    }
+    
+    func testRetrievalConnected() throws {
+        let hrmServiceUUID = CBMUUID(string: "180D")
+        
+        // Set up the devices in range.
+        hrm.simulateMacChange()
+        hrm.simulateProximityChange(.near)
+        blinky.simulateProximityChange(.immediate)
+        thingy.simulateProximityChange(.far)
+        
+        let managerPoweredOn = XCTestExpectation(description: "Powered On")
+        let hrmFound = XCTestExpectation(description: "Found")
+        let otherFound = XCTestExpectation(description: "Other found")
+        otherFound.isInverted = true
+        let hrmRetrieved = XCTestExpectation(description: "Retrieved after service discovery")
+        let hrmNotRetrieved = XCTestExpectation(description: "Not retrieved as not connected")
+        let hrmStillNotRetrieved = XCTestExpectation(description: "Not retrieved as services not discovered")
+        
+        // Create an instance of a mock CBMCentralManager.
+        let manager = CBMCentralManagerMock()
+        
+        // Define the CBMPeripheralDelegate, which will wait until the serivces are
+        // discovered and will try to retrieve the device.
+        let peripheralDelegate = CBMPeripheralDelegateProxy()
+        peripheralDelegate.didDiscoverServices = { peripheral, error in
+            XCTAssertNil(error)
+            
+            // HRM device should now be connected and retrievable.
+            let list = manager.retrieveConnectedPeripherals(withServices: [hrmServiceUUID])
+            XCTAssertFalse(list.isEmpty)
+            
+            if !list.isEmpty {
+                hrmRetrieved.fulfill()
+            }
+        }
+        
+        // Define the CBCentralManagerDelegate, which will wait until the manager is ready
+        // and will check the successful retrieval.
+        let delegate = CBMCentralManagerDelegateProxy()
+        delegate.didUpdateState = { _ in managerPoweredOn.fulfill() }
+        delegate.didDiscoverPeripheral = { manager, peripheral, _, _ in
+            guard peripheral.identifier == hrm.identifier else {
+                otherFound.fulfill()
+                return
+            }
+            hrmFound.fulfill()
+            
+            // HRM device should still not be connected and retrievable.
+            let list = manager.retrieveConnectedPeripherals(withServices: [hrmServiceUUID])
+            XCTAssertTrue(list.isEmpty)
+            
+            if list.isEmpty {
+                hrmNotRetrieved.fulfill()
+            }
+            
+            // Connect to the device when found.
+            peripheral.delegate = peripheralDelegate
+            manager.connect(peripheral)
+        }
+        delegate.didConnect = { manager, peripheral in
+            // HRM device should still not be connected and retrievable.
+            let list = manager.retrieveConnectedPeripherals(withServices: [hrmServiceUUID])
+            XCTAssertTrue(list.isEmpty)
+            
+            if list.isEmpty {
+                hrmStillNotRetrieved.fulfill()
+            }
+            
+            // Discover services.
+            peripheral.discoverServices([hrmServiceUUID])
+        }
+        manager.delegate = delegate
+        
+        // Wait until the manager is initialized.
+        wait(for: [managerPoweredOn], timeout: 1.0)
+        
+        // Initially, none of the devices should be retrievable, as they have not been scanned.
+        let list = manager.retrieveConnectedPeripherals(withServices: [hrmServiceUUID])
+        XCTAssertTrue(list.isEmpty)
+        
+        // Scan only for HRM device. Other devices may also be in range.
+        manager.scanForPeripherals(withServices: [hrmServiceUUID])
+        
+        wait(for: [hrmFound, otherFound, hrmNotRetrieved, hrmStillNotRetrieved, hrmRetrieved], timeout: 1.0)
     }
 
 }
