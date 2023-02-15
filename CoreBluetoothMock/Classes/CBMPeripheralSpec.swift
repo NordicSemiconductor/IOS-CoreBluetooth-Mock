cc1e47752897ef73cb0d05694efecbca0fd8892a
Aleksander Nowakowski <a.f.nowakowski@gmail.com>
2023-02-15 14:46:04 +0100

    Removing unrelated change

--- CoreBluetoothMock/Classes/CBMPeripheralSpec.swift
+++ CoreBluetoothMock/Classes/CBMPeripheralSpec.swift
@@ -76,7 +76,7 @@ public class CBMPeripheralSpec {
     /// The device's advertising data.
     /// Make sure to include `CBAdvertisementDataIsConnectable` if
     /// the device is connectable.
-    public var advertisementData: [String : Any]?
+    public let advertisementData: [String : Any]?
     /// The advertising interval.
     public let advertisingInterval: TimeInterval?
     
